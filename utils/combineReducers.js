4cf0b9933fc1f56aa7a05db04f7462d7d9e03175
Ron Waldon <jokeyrhyme@gmail.com>
2015-08-30 07:51:24 +1000

    healing spells work!

--- utils/combineReducers.js
+++ utils/combineReducers.js
@@ -4,11 +4,21 @@
 @param {Object} reducers
 @returns {Function}
 */
-export default function (reducers = {}) {
+export default function (keyedReducers = {}, ...reducers) {
   return function (state = new Map(), action) {
-    return Object.keys(reducers).reduce((prev, current, index) => {
-      let reducer = reducers[current];
-      return prev.set(current, reducer(prev.get(current), action));
-    }, state);
+    let result = state;
+
+    // run reducers that are specific to top-level keys
+    result = Object.keys(keyedReducers).reduce((prev, key) => {
+      let reducer = keyedReducers[key];
+      return prev.set(key, reducer(prev.get(key), action));
+    }, result);
+
+    // run reducers that access the complete state
+    result = reducers.reduce((prev, reducer) => {
+      return reducer(prev, action);
+    }, result);
+
+    return result;
   };
 }
