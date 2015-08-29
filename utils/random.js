1595ff1887ecc5da4878b3997db5db62b87a912f
Ron Waldon <jokeyrhyme@gmail.com>
2015-08-30 08:05:06 +1000

    de-duplicate randomIndex implementation

--- utils/random.js
+++ utils/random.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// foreign modules
+
+import { Set } from 'immutable';
+
+// this module
+
 export function toNumber (min, max) {
   return Math.floor((Math.random() * (max - min)) + min);
 }
@@ -8,6 +14,12 @@ export function toBoolean () {
   return Math.random() >= 0.5;
 }
 
-export function toIndex (array) {
-  return toNumber(0, array.length);
+export function toIndex (values) {
+  if (Set.isSet(values)) {
+    return toNumber(0, values.size);
+  }
+  if (Array.isArray(values)) {
+    return toNumber(0, values.length);
+  }
+  return 0;
 }
